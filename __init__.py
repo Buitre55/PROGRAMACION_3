3548349b2383624974de2c3ad67d11f151aebcd6
Angel Manuel Buitrago Montes <angel_m_buitrago@hotmail.com>
2017-09-14 10:37:39 -0500

    CLASE #1
    PRACTICAS DE TABLERO

---
+++ __init__.py
@@ -0,0 +1,26 @@
+edad = 20
+
+print(type(edad))
+a = 5
+b = 2
+c = a + b
+d = a - b
+e = 8 * a
+f = 10 / b
+h = a % b
+
+print ("los resultados fueron :")
+print("c = " + str(c))
+print("c = " + str(d))
+print("e = %d" % d)
+print#("f = "+ str(f) + "\ng =" + str(g))
+print("h = " + str(h) + "\n\n Ciao!")
+
+salario = 2000.75
+print ("El salario de hoy es $%.2f" % salario
+
+nombre_completo = "Petra Petrov"
+telefono = '236-5555'
+direcion = """Avenida  Siempre Viva  PH Torre A Apartamento 18Z Panama,Republica de Panama"""
+
+print(nombre_completo, "vive en "+ dirrecion)
